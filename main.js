36f6fa3ac0f388de268e3d405cac771f2f906675
SysPoe <panddplayz@outlook.com>
2021-01-18 17:40:57 +1000

    Updated website

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function onLoad() {
 	if (window.location.hash == "#Instamessage") {
 		Instamessage();
 	}
+	setTimeout(100, scrollLoop);
 }
 function Instamessage() {
 	document.getElementById('nav').style.display = 'none';
@@ -15,4 +16,20 @@ function ProjectsBySyspoe() {
 	document.getElementById('instamessage').style.display = 'none';
 	document.title = "Projects By SysPoe";
 	window.location.hash = "";
+}
+
+var xScrollPosition;
+var yScrollPosition;
+
+function scrollLoop() {
+	xScrollPosition = window.scrollX;
+	yScrollPosition = window.scrollY;
+
+	setTranslate(0, yScrollPosition * -0.2, document.getElementById("test"));
+
+	requestAnimationFrame(scrollLoop);
+}
+
+function setTranslate(xPos, yPos, el) {
+	el.style.transform = 'translate3d(' + xPos + 'px,' + yPos + 'px,0)';
 }
